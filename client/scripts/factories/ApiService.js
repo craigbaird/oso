6d499fffdc474df382b4dd4bbb7ac4e62b5c1f20
Craig Baird <bairdcraig@gmail.com>
2017-10-19 20:30:41 -0500

    progress displaying google books data

--- client/scripts/factories/ApiService.js
+++ client/scripts/factories/ApiService.js
@@ -1,5 +1,6 @@
 myApp.factory('ApiService', ['$http', '$location', function($http, $location){
   // console.log('Api Service Loaded');
+    // var bookInfoFromApi = {};
     var bookInfoFromApi = {};
     return {
         bookInfoFromApi : bookInfoFromApi,
@@ -8,6 +9,21 @@ myApp.factory('ApiService', ['$http', '$location', function($http, $location){
             $http.get('/googleBooksApi/'+ isbn).then(function(response){
                 bookInfoFromApi.response = response.data;
                 console.log('book results: ', response.data);
+
+                // Break the object into parts
+                bookInfoFromApi.allowAnonLogging = response.data.items[0].volumeInfo.allowAnonLogging;
+                bookInfoFromApi.authors = response.data.items[0].volumeInfo.authors[0];
+                bookInfoFromApi.averageRating = response.data.items[0].volumeInfo.averageRating;
+                bookInfoFromApi.canonicalVolumeLink = response.data.items[0].volumeInfo.canonicalVolumeLink;
+                bookInfoFromApi.categories = response.data.items[0].volumeInfo.categories[0];
+                bookInfoFromApi.contentVersion = response.data.items[0].volumeInfo.contentVersion;
+                bookInfoFromApi.description = response.data.items[0].volumeInfo.description;
+                // industryIdentifiers goes here
+                bookInfoFromApi.infoLink = response.data.items[0].volumeInfo.infoLink;
+                // Left off here
+                bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
+                bookInfoFromApi.pageCount = response.data.items[0].volumeInfo.pageCount;
+                bookInfoFromApi.publishedDate = response.data.items[0].volumeInfo.publishedDate;
 
                 $location.path('/bookResults');
             });
