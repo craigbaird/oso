205f15b13346ea560d898fda3da7d91747906abf
Craig Baird <bairdcraig@gmail.com>
2017-10-20 19:27:59 -0500

    all volume info displayed

--- client/scripts/factories/ApiService.js
+++ client/scripts/factories/ApiService.js
@@ -1,6 +1,5 @@
 myApp.factory('ApiService', ['$http', '$location', function($http, $location){
   // console.log('Api Service Loaded');
-    // var bookInfoFromApi = {};
     var bookInfoFromApi = {};
     return {
         bookInfoFromApi : bookInfoFromApi,
@@ -10,7 +9,7 @@ myApp.factory('ApiService', ['$http', '$location', function($http, $location){
                 bookInfoFromApi.response = response.data;
                 console.log('book results: ', response.data);
 
-                // Break the object into parts
+                // Break the object into easier to read parts
                 bookInfoFromApi.allowAnonLogging = response.data.items[0].volumeInfo.allowAnonLogging;
                 bookInfoFromApi.authors = response.data.items[0].volumeInfo.authors[0];
                 bookInfoFromApi.averageRating = response.data.items[0].volumeInfo.averageRating;
@@ -18,12 +17,18 @@ myApp.factory('ApiService', ['$http', '$location', function($http, $location){
                 bookInfoFromApi.categories = response.data.items[0].volumeInfo.categories[0];
                 bookInfoFromApi.contentVersion = response.data.items[0].volumeInfo.contentVersion;
                 bookInfoFromApi.description = response.data.items[0].volumeInfo.description;
-                // industryIdentifiers goes here
+                // industry identifiers go here
                 bookInfoFromApi.infoLink = response.data.items[0].volumeInfo.infoLink;
-                // Left off here
-                bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
+                bookInfoFromApi.language = response.data.items[0].volumeInfo.language;
                 bookInfoFromApi.pageCount = response.data.items[0].volumeInfo.pageCount;
+                bookInfoFromApi.maturityRating = response.data.items[0].volumeInfo.maturityRating;
+                bookInfoFromApi.previewLink = response.data.items[0].volumeInfo.previewLink;
+                bookInfoFromApi.printType = response.data.items[0].volumeInfo.printType;
                 bookInfoFromApi.publishedDate = response.data.items[0].volumeInfo.publishedDate;
+                bookInfoFromApi.publisher = response.data.items[0].volumeInfo.publisher;
+                bookInfoFromApi.ratingsCount = response.data.items[0].volumeInfo.ratingsCount;
+                // reading modes go here
+                bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
 
                 $location.path('/bookResults');
             });
