33eadc68dab3966b0a5311a7002c2cbe5d15b1b7
Craig Baird <bairdcraig@gmail.com>
2017-10-21 16:26:08 -0500

    api info displayed

--- client/scripts/controllers/bookResultsController.js
+++ client/scripts/controllers/bookResultsController.js
@@ -2,26 +2,26 @@ myApp.controller('BookResultsController', ['$scope', '$http', '$location', 'User
   console.log('Book Results Controller loaded');
   $scope.logout = UserService.logout;
 
-  // $scope.addBook = function(bookShelfObject) {
-  //     console.log('Adding Book Info: ', bookShelfObject);
-  //     var book = bookShelfObject;
-  //
-  //     $http.post('/myBookShelf', bookShelfObject).then(function(response){
-  //       getBookShelf();
-  //
-  //       /////// ??? ///////
-  //       $scope.book.name = "";
-  //     });
-  //   };
-  //
-  //   var getBookShelf = function() {
-  //     $http.get('/myBookShelf').then(function(response){
-  //       console.log("All Current Books: ", response);
-  //       $scope.list = response.data;
-  //        console.log(response.data);
-  //     });
-  //   };
-  //   getBookShelf();
+  $scope.addBook = function(bookShelfObject) {
+      console.log('Sending Book Info To Database: ', bookShelfObject);
+    //   var book = bookShelfObject;
+
+    //   $http.post('/myBookShelf', bookShelfObject).then(function(response){
+    //     getBookShelf();
+      //
+    //     /////// ??? ///////
+    //     $scope.book.name = "";
+    //   });
+    };
+
+    // var getBookShelf = function() {
+    //   $http.get('/myBookShelf').then(function(response){
+    //     console.log("All Current Books: ", response);
+    //     $scope.list = response.data;
+    //      console.log(response.data);
+    //   });
+    // };
+    // getBookShelf();
 
   $scope.bookInfoFromApi = ApiService.bookInfoFromApi;
 
