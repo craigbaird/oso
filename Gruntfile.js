3f178ba0a5cda428d92e88598254f88247362870
Craig Baird <bairdcraig@gmail.com>
2017-11-07 20:59:45 -0600

    changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    browserify: {
-        dist: {
-            files: {
-                // destination for transpiled js : source js
-                'dist/myproject.js': 'src/index.es6'
-            },
-            options: {
-                transform: [['babelify', { presets: "es2015" }]],
-                browserifyOptions: {
-                    debug: true
-                }
-            }
-        }
-    },
+    // browserify: {
+    //     dist: {
+    //         files: {
+    //             // destination for transpiled js : source js
+    //             'dist/myproject.js': 'src/index.es6'
+    //         },
+    //         options: {
+    //             transform: [['babelify', { presets: "es2015" }]],
+    //             browserifyOptions: {
+    //                 debug: true
+    //             }
+    //         }
+    //     }
+    // },
     uglify: {
       build: {
         src: ['client/scripts/*.js',
@@ -52,13 +52,6 @@ module.exports = function(grunt){
               'angular-route.min.js.map'],
         dest: 'server/public/vendors/angular-route/'
       },
-    //   sweetAlert2: {
-    //     expand: true,
-    //     cwd: 'node_modules/sweetalert2/dist/',
-    //     src: ['sweetalert2.min.css',
-    //           'sweetalert2.min.js'],
-    //     dest: 'server/public/vendors/sweetalert2/'
-    //   }
    }, // end copy
     watch: {
       files: [
@@ -68,10 +61,10 @@ module.exports = function(grunt){
     }
   });
 
-  grunt.loadNpmTasks('grunt-browserify');
+  // grunt.loadNpmTasks('browserify:dist');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['browserify:dist', 'uglify', 'copy', 'watch']);
+  grunt.registerTask('default', ['uglify', 'copy', 'watch']);
 };
