d7265ca22fe6356600f8080fbc0e574607f0a5fe
Craig Baird <bairdcraig@gmail.com>
2017-11-11 21:43:09 -0600

    progress

--- client/scripts/controllers/myBookShelfController.js
+++ client/scripts/controllers/myBookShelfController.js
@@ -2,6 +2,8 @@ myApp.controller('MyBookShelfController', ['$scope', '$http', '$location', 'User
   // console.log('MyBookShelfController loaded');
   $scope.logout = UserService.logout;
 
+  var bookShelf = {};
+
   $scope.findBook = function() {
       // var isbn is the input from user in the search field and the only accepted way to search
       // will need to add a way to allow for other input types
@@ -10,10 +12,11 @@ myApp.controller('MyBookShelfController', ['$scope', '$http', '$location', 'User
       ApiService.getBooks(isbn);
   };
 
-  getBookShelf();
-  var getBookShelf = function() {
-    $http.get('./books').then(function(response){
-      $scope.list = response.data;
+  displayBookShelf();
+  var displayBookShelf = function() {
+    $http.get('/books').then(function(response){
+      console.log('all current books: ', response.data);
+      bookShelf.list = response.data;
       console.log(response.data);
     });
   };
