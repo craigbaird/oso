cdc70dd1e94ff08a765f0b66f1f71a4495caf6c0
Craig Baird <bairdcraig@gmail.com>
2017-12-26 21:20:59 -0600

    progress

--- server/public/scripts/controllers/myBookShelfController.js
+++ server/public/scripts/controllers/myBookShelfController.js
@@ -6,30 +6,31 @@ myApp.controller('MyBookShelfController', ['$scope', '$http', '$location', 'User
   MyBookShelfService.getBookShelf();
 
   $scope.findBook = function() {
-      // var isbn is the input from user in the search field and the only accepted way to search
-      // will need to add a way to allow for other input types
-      var isbn = $scope.bookToSearch.name;
-      ApiService.getBooks(isbn);
+    // var isbn is the input from user in the search field and the only accepted way to search
+    // will need to add a way to allow for other input types
+    var isbn = $scope.bookToSearch.name;
+    ApiService.getBooks(isbn);
   };
 
   $scope.editComments = function(bookObject) {
     console.log("Edit comments clicked");
     appendInput();
-    MyBookShelfService.editMyComments(bookObject);
+    // this needs to be moved to a new function
+    // MyBookShelfService.editMyComments(bookObject);
   };
 
   var counter = 0;
   var limit = 1;
   function appendInput() {
     if (counter == limit)  {
-          alert("You have reached the limit of adding " + counter + " inputs");
-     }
-     else {
-          var myComments = document.createElement('div');
-          myComments.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
-          document.getElementById(myComments).appendChild(myComments);
-          counter++;
-     }
+      alert("You have reached the limit of adding " + counter + " inputs");
+      }
+      else {
+        var myComments = document.createElement('div');
+        myComments.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+        document.getElementById(myComments).appendChild(myComments);
+        counter++;
+      }
   }
 
   $scope.deleteBook = function(bookObject){
