d8a8b36877712a0062dfe8fd05faf23f39fd15cb
Craig Baird <bairdcraig@gmail.com>
2018-02-25 21:32:22 -0600

    progress

--- server/public/scripts/factories/MyBookShelfService.js
+++ server/public/scripts/factories/MyBookShelfService.js
@@ -3,6 +3,36 @@ myApp.factory('MyBookShelfService', ['$http', '$location', function($http, $loca
 
     let getBookShelf = () => {
       $http.get('/books').then(function(response) {
+        console.log('response.data', response.data);
+        var bookList = response.data;
+        var newList = [];
+        bookList.forEach(function(book, index) {
+          newList.push({book.authors,
+                       book.categories,
+                       book.description,
+                       book.flagged,
+                       book.isbn10,
+                       book.isbn13,
+                       book.language,
+                       book.my_comments,
+                       book.page_count,
+                       book.published_date,
+                       book.publisher,
+                       book.small_thumbnail,
+                       book.thumbnail,
+                       book.title});
+          // if (undefined != book._id ) {
+          //   delete book._id;
+          // }
+          // if (undefined != book.user_id) {
+          //   delete book.user_id;
+          // }
+          // if (undefined != book._v) {
+          //   delete book._v;
+          // }
+        });
+        // console.log("bookList", bookList);
+        console.log('newList', newList);
         bookShelf.list = response.data;
       });
     };
