310d29e560669fa5658abcf74077cad665c3bfb6
Craig Baird <bairdcraig@gmail.com>
2018-03-02 21:24:55 -0600

    progress

--- server/public/scripts/factories/MyBookShelfService.js
+++ server/public/scripts/factories/MyBookShelfService.js
@@ -3,37 +3,28 @@ myApp.factory('MyBookShelfService', ['$http', '$location', function($http, $loca
 
     let getBookShelf = () => {
       $http.get('/books').then(function(response) {
-        console.log('response.data', response.data);
         var bookList = response.data;
+        console.log('bookList', bookList);
 
         // don't delete properties, user_id needed for functionality
         // filter in the view
-        bookList.forEach(function(book, index) {
-          if (book.user_id) {
-            delete book.user_id;
-          }
-          if (book._id) {
-            delete book._id;
-          }
-          if (book.__v) {
-            console.log(book.__v);
-            delete book.__v;
-          }
-          // if (undefined != book._id ) {
-          //   delete book._id;
-          // }
-          // if (undefined != book.user_id) {
-          //   delete book.user_id;
-          // }
-          // if (undefined != book._v) {
-          //   delete book._v;
-          // }
-        });
-        console.log('bookList', bookList)
+
+        // bookList.forEach(function(book, index) {
+        //   if (book.user_id) {
+        //     delete book.user_id;
+        //   }
+        //   if (book._id) {
+        //     delete book._id;
+        //   }
+        //   if (book.__v) {
+        //     console.log(book.__v);
+        //     delete book.__v;
+        //   }
 
         bookShelf.list = response.data;
-      });
+        });
     };
+
 
     let editMyComments = (bookObject) => {
       $http.put('/books/editMyComments', bookObject).then(function(response) {
--- server/routes/books.js
+++ server/routes/books.js
@@ -18,9 +18,7 @@ var MyBookShelfSchema = mongoose.Schema({
   'user_id': String,
   'my_comments': String,
   'flagged': String
-},
-{versionKey: false}
-);
+});
 
 var Books = mongoose.model('Books', MyBookShelfSchema);
 
@@ -28,16 +26,6 @@ router.get('/', function(req, res) {
   if(req.isAuthenticated()) {
     var user = req.user._id;
     Books.find({user_id : user}, function(err, allBooks){
-      // console.log(allBooks);
-
-
-      // allBooks.forEach(function(book, index) {
-      //     if (book.hasOwnProperty('_v')){
-      //       console.log(book._v);
-      //     }
-      // });
-
-
       if (err){
         console.log(err);
         res.sendStatus(500);
