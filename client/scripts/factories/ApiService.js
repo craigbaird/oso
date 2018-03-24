6d508ed6c47f857283cdf534b8aa79d60a8ed5d4
Craig Baird <bairdcraig@gmail.com>
2018-03-23 21:20:52 -0500

    breaking down object from API and beginning to loop through parts

--- client/scripts/factories/ApiService.js
+++ client/scripts/factories/ApiService.js
@@ -18,54 +18,39 @@ myApp.factory('ApiService', ['$http', '$location', function($http, $location){
         bookInfoFromApi : bookInfoFromApi,
         getBooks : function(isbn){
             $http.get('/googleBooksApi/'+ isbn).then(function(response){
-                bookInfoFromApi.response = response.data;
-
-                // TRYING THIS
-                // var items = response.data.items[0].volumeInfo;
-                //   for (var item in items) {
-                //     bookInfo.push(item);
-                //   }
-                //   console.log(bookInfo);
-
-                // AND THIS
-                // var arr = [response.data.items];
-                //
-                // arr.forEach(function(element) {
-                //   console.log('element', element);
-                // });
-
-                // AND THIS
-                // let items  = response.data.items[0].volumeInfo;
-                //
-                // items.forEach(function(element) {
-                //   console.log(element);
-                // });
+                var allGoogleBooksResults = response.data;
+                var volumeInfo = [];
+                allGoogleBooksResults.items.forEach(function(item, index) {
+                    volumeInfo.push(item.volumeInfo);
+                });
+                bookInfoFromApi.volumeInfo = volumeInfo;
+                console.log('bookInfoFromApi', bookInfoFromApi);
 
 
-                // Break the object into easier to read parts
-                bookInfoFromApi.thumbnail = response.data.items[0].volumeInfo.imageLinks.thumbnail;
-                bookInfoFromApi.smallThumbnail = response.data.items[0].volumeInfo.imageLinks.smallThumbnail;
-                bookInfoFromApi.allowAnonLogging = response.data.items[0].volumeInfo.allowAnonLogging;
-                bookInfoFromApi.authors = response.data.items[0].volumeInfo.authors[0];
-                bookInfoFromApi.averageRating = response.data.items[0].volumeInfo.averageRating;
-                bookInfoFromApi.canonicalVolumeLink = response.data.items[0].volumeInfo.canonicalVolumeLink;
-                bookInfoFromApi.categories = response.data.items[0].volumeInfo.categories[0];
-                bookInfoFromApi.contentVersion = response.data.items[0].volumeInfo.contentVersion;
-                bookInfoFromApi.description = response.data.items[0].volumeInfo.description;
-                // Industry identifiers go here
-                bookInfoFromApi.isbn10 = response.data.items[0].volumeInfo.industryIdentifiers[0].identifier;
-                bookInfoFromApi.isbn13 = response.data.items[0].volumeInfo.industryIdentifiers[1].identifier;
-                bookInfoFromApi.infoLink = response.data.items[0].volumeInfo.infoLink;
-                bookInfoFromApi.language = response.data.items[0].volumeInfo.language;
-                bookInfoFromApi.pageCount = response.data.items[0].volumeInfo.pageCount;
-                bookInfoFromApi.maturityRating = response.data.items[0].volumeInfo.maturityRating;
-                bookInfoFromApi.previewLink = response.data.items[0].volumeInfo.previewLink;
-                bookInfoFromApi.printType = response.data.items[0].volumeInfo.printType;
-                bookInfoFromApi.publishedDate = response.data.items[0].volumeInfo.publishedDate;
-                bookInfoFromApi.publisher = response.data.items[0].volumeInfo.publisher;
-                bookInfoFromApi.ratingsCount = response.data.items[0].volumeInfo.ratingsCount;
-                // Reading modes go here
-                bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
+                // // Break the object into easier to read parts
+                // bookInfoFromApi.thumbnail = response.data.items[0].volumeInfo.imageLinks.thumbnail;
+                // bookInfoFromApi.smallThumbnail = response.data.items[0].volumeInfo.imageLinks.smallThumbnail;
+                // bookInfoFromApi.allowAnonLogging = response.data.items[0].volumeInfo.allowAnonLogging;
+                // bookInfoFromApi.authors = response.data.items[0].volumeInfo.authors[0];
+                // bookInfoFromApi.averageRating = response.data.items[0].volumeInfo.averageRating;
+                // bookInfoFromApi.canonicalVolumeLink = response.data.items[0].volumeInfo.canonicalVolumeLink;
+                // bookInfoFromApi.categories = response.data.items[0].volumeInfo.categories[0];
+                // bookInfoFromApi.contentVersion = response.data.items[0].volumeInfo.contentVersion;
+                // bookInfoFromApi.description = response.data.items[0].volumeInfo.description;
+                // // Industry identifiers go here
+                // bookInfoFromApi.isbn10 = response.data.items[0].volumeInfo.industryIdentifiers[0].identifier;
+                // bookInfoFromApi.isbn13 = response.data.items[0].volumeInfo.industryIdentifiers[1].identifier;
+                // bookInfoFromApi.infoLink = response.data.items[0].volumeInfo.infoLink;
+                // bookInfoFromApi.language = response.data.items[0].volumeInfo.language;
+                // bookInfoFromApi.pageCount = response.data.items[0].volumeInfo.pageCount;
+                // bookInfoFromApi.maturityRating = response.data.items[0].volumeInfo.maturityRating;
+                // bookInfoFromApi.previewLink = response.data.items[0].volumeInfo.previewLink;
+                // bookInfoFromApi.printType = response.data.items[0].volumeInfo.printType;
+                // bookInfoFromApi.publishedDate = response.data.items[0].volumeInfo.publishedDate;
+                // bookInfoFromApi.publisher = response.data.items[0].volumeInfo.publisher;
+                // bookInfoFromApi.ratingsCount = response.data.items[0].volumeInfo.ratingsCount;
+                // // Reading modes go here
+                // bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
 
                 $location.path('/bookResults');
             });
