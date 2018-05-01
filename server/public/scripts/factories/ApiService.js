e83c3939d31587fcfa6a654506f73216efb61880
Craig Baird <bairdcraig@gmail.com>
2018-04-30 20:32:47 -0500

    progress displaying data from api

--- server/public/scripts/factories/ApiService.js
+++ server/public/scripts/factories/ApiService.js
@@ -15,29 +15,27 @@ myApp.factory('ApiService', ['$http', '$location', function($http, $location){
 
                 // // Break the object into easier to read parts
 
+                bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
+                bookInfoFromApi.authors = response.data.items[0].volumeInfo.authors;
+                bookInfoFromApi.categories = response.data.items[0].volumeInfo.categories;
+                bookInfoFromApi.description = response.data.items[0].volumeInfo.description;
+                bookInfoFromApi.industryIdentifiers = response.data.items[0].volumeInfo.industryIdentifiers;
+                bookInfoFromApi.language = response.data.items[0].volumeInfo.language;
+                bookInfoFromApi.pageCount = response.data.items[0].volumeInfo.pageCount;
+                bookInfoFromApi.publishedDate = response.data.items[0].volumeInfo.publishedDate;
+                bookInfoFromApi.publisher = response.data.items[0].volumeInfo.publisher;
                 // bookInfoFromApi.thumbnail = response.data.items[0].volumeInfo.imageLinks.thumbnail;
                 // bookInfoFromApi.smallThumbnail = response.data.items[0].volumeInfo.imageLinks.smallThumbnail;
                 // bookInfoFromApi.allowAnonLogging = response.data.items[0].volumeInfo.allowAnonLogging;
-                bookInfoFromApi.authors = response.data.items[0].volumeInfo.authors;
                 // bookInfoFromApi.averageRating = response.data.items[0].volumeInfo.averageRating;
                 // bookInfoFromApi.canonicalVolumeLink = response.data.items[0].volumeInfo.canonicalVolumeLink;
-                bookInfoFromApi.categories = response.data.items[0].volumeInfo.categories;
                 // bookInfoFromApi.contentVersion = response.data.items[0].volumeInfo.contentVersion;
-                // bookInfoFromApi.description = response.data.items[0].volumeInfo.description;
-                // // Industry identifiers go here
-                // bookInfoFromApi.isbn10 = response.data.items[0].volumeInfo.industryIdentifiers[0].identifier;
-                // bookInfoFromApi.isbn13 = response.data.items[0].volumeInfo.industryIdentifiers[1].identifier;
                 // bookInfoFromApi.infoLink = response.data.items[0].volumeInfo.infoLink;
-                // bookInfoFromApi.language = response.data.items[0].volumeInfo.language;
-                // bookInfoFromApi.pageCount = response.data.items[0].volumeInfo.pageCount;
                 // bookInfoFromApi.maturityRating = response.data.items[0].volumeInfo.maturityRating;
                 // bookInfoFromApi.previewLink = response.data.items[0].volumeInfo.previewLink;
                 // bookInfoFromApi.printType = response.data.items[0].volumeInfo.printType;
-                // bookInfoFromApi.publishedDate = response.data.items[0].volumeInfo.publishedDate;
-                // bookInfoFromApi.publisher = response.data.items[0].volumeInfo.publisher;
                 // bookInfoFromApi.ratingsCount = response.data.items[0].volumeInfo.ratingsCount;
                 // // Reading modes go here
-                // bookInfoFromApi.title = response.data.items[0].volumeInfo.title;
 
                 $location.path('/bookResults');
             });
